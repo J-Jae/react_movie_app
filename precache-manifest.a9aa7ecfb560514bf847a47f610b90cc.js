aa2ceedb6fbbc39ff53531a8f5ecbc5db7b9f288
J-Jae <dotori0807@gmail.com>
2020-07-11 21:39:18 +0900

    Updates

---
+++ precache-manifest.a9aa7ecfb560514bf847a47f610b90cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900a3059614352f5c2f2df13b7c2f5dd",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "3a9503f23128c260c0ff",
+    "url": "/react_movie_app/static/css/main.93ea6e11.chunk.css"
+  },
+  {
+    "revision": "b465fb3a41db387c5800",
+    "url": "/react_movie_app/static/js/2.6760420d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movie_app/static/js/2.6760420d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9503f23128c260c0ff",
+    "url": "/react_movie_app/static/js/main.a4c829d6.chunk.js"
+  },
+  {
+    "revision": "00ec9c2259bc5a9a77bf",
+    "url": "/react_movie_app/static/js/runtime-main.7e6802fe.js"
+  }
+]);
